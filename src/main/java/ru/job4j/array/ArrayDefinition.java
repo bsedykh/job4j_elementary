bbff9b4df75be725a9b95fab7bc7083d66b3c50c
Boris Sedykh <sedykh.boris@yandex.ru>
2023-10-02 09:39:56 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -9,5 +9,15 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + surnames.length);
         float[] prices = new float[40];
         System.out.println("Размер массива равен: " + prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Petr";
+        names[1] = "Ivan";
+        names[2] = "Dmitry";
+        names[3] = "Sergey";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
